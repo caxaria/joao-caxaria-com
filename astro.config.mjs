111a294fbb2e20b379857c71ac96d331b71e8ed3
João Caxaria <joao.caxaria@coverflex.com>
2024-11-15 21:37:04 +0000

    Added CF deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://joao-caxaria-com.netlify.app',
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
 });
